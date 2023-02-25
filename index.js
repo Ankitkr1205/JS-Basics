28fa31a85bf37eb442d2d2a1f27f11b2d5382496
Ankit Chaudhary <chaudharyankit573nets@gmail.com>
2023-02-25 20:31:02 +0530

    added main pull

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-//Java script object
-// 1. Creating an object 
-// 2. Updating an Object
-// 3. Deleting an object
-
-const myOBJ={};
-myOBJ["ankit"]=20;
-console.log(myOBJ)
-
-myOBJ["prashant"]=myOBJ["ankit"]
-//delete myOBJ["ankit"]
-console.log(myOBJ);
+console.log("hiii ")
